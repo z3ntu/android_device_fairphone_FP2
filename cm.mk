a48458114d1270c9466c14bd8e1812e6b4ad34bb
Roberto MF <roboe@virgulilla.com>
2016-10-22 22:48:41 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := fp2
+PRODUCT_RELEASE_NAME := FP2
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/fairphone/fp2/device_fp2.mk)
+$(call inherit-product, device/fairphone/fp2/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fp2
 PRODUCT_NAME := cm_fp2
-PRODUCT_BRAND := fairphone
-PRODUCT_MODEL := fp2
-PRODUCT_MANUFACTURER := fairphone
+PRODUCT_BRAND := Fairphone
+PRODUCT_MODEL := FP2
+PRODUCT_MANUFACTURER := Fairphone
--- device.mk
+++ device.mk
@@ -8,22 +8,9 @@ $(call inherit-product-if-exists, vendor/fairphone/fp2/fp2-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/fairphone/fp2/overlay
 
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/fairphone/fp2/prebuilts/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-
-LOCAL_KERNEL_DT := device/fairphone/fp2/prebuilts/dt.img
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL_DT):dt.img
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
 PRODUCT_NAME := full_fp2
 PRODUCT_DEVICE := fp2
